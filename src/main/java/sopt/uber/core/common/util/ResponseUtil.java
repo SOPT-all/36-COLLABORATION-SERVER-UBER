b74c4952c831e2cf203c00d5673b063c7c9cc44e
daeun-han <111632494+daeun-han@users.noreply.github.com>
2025-05-13 22:03:45 +0900

    [refactor] ResponseUtil 응답 타입에 제네릭 적용

--- src/main/java/sopt/uber/core/common/util/ResponseUtil.java
+++ src/main/java/sopt/uber/core/common/util/ResponseUtil.java
@@ -7,16 +7,12 @@ import sopt.uber.core.common.response.CommonResponse;
 public class ResponseUtil {
 
     // 성공 응답
-    public static <T> ResponseEntity<Object> success(T data) {
-        if (data == null) { // data가 없을 경우
-            return new ResponseEntity<>(new CommonResponse<>(200, "요청이 성공적으로 처리되었습니다."), HttpStatus.OK);
-        } else { // data가 있을 경우
+    public static <T> ResponseEntity<CommonResponse<T>> success(T data) {
             return new ResponseEntity<>(new CommonResponse<>(200, "요청이 성공적으로 처리되었습니다.", data), HttpStatus.OK);
-        }
     }
 
     // 실패 응답
-    public static ResponseEntity<Object> fail(HttpStatus status, String message) {
+    public static <T> ResponseEntity<CommonResponse<T>> fail(HttpStatus status, String message) {
         return new ResponseEntity<>(new CommonResponse<>(status.value(), message), status);
     }
 }

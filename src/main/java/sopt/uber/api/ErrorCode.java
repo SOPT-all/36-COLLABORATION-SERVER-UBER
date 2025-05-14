795c443496323464a6d3ca8791f35eac196c770a
dajinYang <didekwls0104@naver.com>
2025-05-15 00:51:09 +0900

    [feat] ErrorCode 생성

---
+++ src/main/java/sopt/uber/api/ErrorCode.java
@@ -0,0 +1,26 @@
+package sopt.uber.api;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 요청 형식을 확인해주세요."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "지원하지 않는 URL입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP method 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String msg;
+
+    ErrorCode(HttpStatus httpStatus, String msg) {
+        this.httpStatus = httpStatus;
+        this.msg = msg;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}

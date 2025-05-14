449352548ee2a583d32118136d683249821ef154
dajinYang <didekwls0104@naver.com>
2025-05-15 01:15:59 +0900

    [refactor] 200요청 값, HttpStatus.Ok값으로 변경

--- src/main/java/sopt/uber/core/common/util/ResponseUtil.java
+++ src/main/java/sopt/uber/core/common/util/ResponseUtil.java
@@ -8,7 +8,7 @@ public class ResponseUtil {
 
     // 성공 응답
     public static <T> ResponseEntity<CommonResponse<T>> success(T data) {
-            return new ResponseEntity<>(new CommonResponse<>(200, "요청이 성공적으로 처리되었습니다.", data), HttpStatus.OK);
+        return new ResponseEntity<>(new CommonResponse<>(HttpStatus.OK.value(), "요청이 성공적으로 처리되었습니다.", data), HttpStatus.OK);
     }
 
     // 실패 응답

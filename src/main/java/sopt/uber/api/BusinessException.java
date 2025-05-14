02d1fe2abaaaebe4ddd3e6052bb3ec97256035b6
dajinYang <didekwls0104@naver.com>
2025-05-15 00:58:29 +0900

    [feat] BusinessException 생성

---
+++ src/main/java/sopt/uber/api/BusinessException.java
@@ -0,0 +1,14 @@
+package sopt.uber.api;
+
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMsg());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getFailCode() {
+        return errorCode;
+    }
+}

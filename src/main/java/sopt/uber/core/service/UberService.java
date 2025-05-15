d678042151ca4ebd28e2120933c81cfb1dd8eb24
daeunhan <daeunhan0723@gmail.com>
2025-05-15 21:51:49 +0900

    [feat] Uber service 생성

---
+++ src/main/java/sopt/uber/core/service/UberService.java
@@ -0,0 +1,29 @@
+package sopt.uber.core.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sopt.uber.api.dto.req.UberReq;
+import sopt.uber.api.exception.BusinessException;
+import sopt.uber.api.exception.ErrorCode;
+import sopt.uber.core.domain.Uber;
+import sopt.uber.core.repository.UberRepository;
+
+@Service
+@Transactional
+public class UberService {
+
+    private final UberRepository uberRepository;
+
+    public UberService(UberRepository uberRepository) {
+        this.uberRepository = uberRepository;
+    }
+
+    public void createUber(UberReq req) {
+        if (req.departures().equals(req.destination())) {
+            throw new BusinessException(ErrorCode.SAME_LOCATION);
+        }
+
+        Uber uber = new Uber(req.departures(), req.destination());
+        uberRepository.save(uber);
+    }
+}

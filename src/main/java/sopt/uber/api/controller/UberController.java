cd537302890ba6d57d642f617b3276fc453fa676
daeunhan <daeunhan0723@gmail.com>
2025-05-15 21:50:53 +0900

    [feat] Uber controller 생성

---
+++ src/main/java/sopt/uber/api/controller/UberController.java
@@ -0,0 +1,25 @@
+package sopt.uber.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sopt.uber.api.dto.req.UberReq;
+import sopt.uber.core.common.response.CommonResponse;
+import sopt.uber.core.common.util.ResponseUtil;
+import sopt.uber.core.service.UberService;
+
+@RestController
+@RequestMapping("/uber")
+public class UberController {
+
+    private final UberService uberService;
+
+    public UberController(UberService uberService) {
+        this.uberService = uberService;
+    }
+
+    @PostMapping("/location")
+    public ResponseEntity<CommonResponse<Void>> createUber(@RequestBody UberReq req) {
+        uberService.createUber(req);
+        return ResponseUtil.success(null);
+    }
+}

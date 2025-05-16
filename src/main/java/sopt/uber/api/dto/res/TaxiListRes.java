17eb0a12721912d5c5627a5c81100661564505bc
daeunhan <daeunhan0723@gmail.com>
2025-05-16 13:06:01 +0900

    [feat] Taxi domain과 dto 생성

---
+++ src/main/java/sopt/uber/api/dto/res/TaxiListRes.java
@@ -0,0 +1,5 @@
+package sopt.uber.api.dto.res;
+
+import java.util.List;
+
+public record TaxiListRes(List<TaxiRes> taxiList) {}
---
+++ src/main/java/sopt/uber/core/domain/Taxi.java
@@ -0,0 +1,31 @@
+package sopt.uber.core.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Taxi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private int min;
+    private int max;
+    private int guests;
+    private String comment;
+
+    public Taxi(long id, String type, int min, int max, int guests, String comment) {
+        this.id = id;
+        this.type = type;
+        this.min = min;
+        this.max = max;
+        this.guests = guests;
+        this.comment = comment;
+    }
+
+    public Taxi() {
+
+    }
+}

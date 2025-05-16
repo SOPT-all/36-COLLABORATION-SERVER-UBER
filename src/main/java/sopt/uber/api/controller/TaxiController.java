fd2c5bb245afd4d16f586e3dd7c6a9980554fd60
daeunhan <daeunhan0723@gmail.com>
2025-05-16 13:06:28 +0900

    [feat] Taxi get api 생성

---
+++ src/main/java/sopt/uber/api/controller/TaxiController.java
@@ -0,0 +1,27 @@
+package sopt.uber.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sopt.uber.api.dto.res.TaxiListRes;
+import sopt.uber.core.common.response.CommonResponse;
+import sopt.uber.core.common.util.ResponseUtil;
+import sopt.uber.core.domain.Taxi;
+import sopt.uber.core.service.TaxiService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/uber")
+public class TaxiController {
+
+    private final TaxiService taxiService;
+
+    public TaxiController(TaxiService taxiService) {
+        this.taxiService = taxiService;
+    }
+
+    @GetMapping("/taxi")
+    public ResponseEntity<CommonResponse<TaxiListRes>> getTaxiList() {
+        TaxiListRes taxiListRes = taxiService.getTaxiList();
+        return ResponseUtil.success(taxiListRes);
+    }
+}
---
+++ src/main/java/sopt/uber/core/repository/TaxiRepository.java
@@ -0,0 +1,17 @@
+package sopt.uber.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import sopt.uber.api.dto.res.TaxiListRes;
+import sopt.uber.api.dto.res.TaxiRes;
+import sopt.uber.core.domain.Taxi;
+
+import java.util.List;
+
+@Repository
+public interface TaxiRepository extends JpaRepository<Taxi, Long> {
+
+    @Query("SELECT new sopt.uber.api.dto.res.TaxiRes(t.id, t.type, t.min, t.max, t.guests, t.comment) FROM Taxi t")
+    List<TaxiRes> findAllTaxiListResponse();
+}

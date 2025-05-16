64887a1d53a0633ee057bd02d88764e8369f6e11
daeunhan <daeunhan0723@gmail.com>
2025-05-17 00:36:07 +0900

    [fix] id값에 따라 TaxiList 구성

--- src/main/java/sopt/uber/core/repository/TaxiRepository.java
+++ src/main/java/sopt/uber/core/repository/TaxiRepository.java
@@ -3,7 +3,6 @@ package sopt.uber.core.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import sopt.uber.api.dto.res.TaxiListRes;
 import sopt.uber.api.dto.res.TaxiRes;
 import sopt.uber.core.domain.Taxi;
 
@@ -12,6 +11,11 @@ import java.util.List;
 @Repository
 public interface TaxiRepository extends JpaRepository<Taxi, Long> {
 
-    @Query("SELECT new sopt.uber.api.dto.res.TaxiRes(t.id, t.type, t.min, t.max, t.guests, t.comment) FROM Taxi t")
+    @Query("SELECT new sopt.uber.api.dto.res.TaxiRes(t.id, t.type, t.min, t.max, t.guests, t.comment) " +
+            "FROM Taxi t WHERE t.id IN (1, 2)")
     List<TaxiRes> findAllTaxiListResponse();
+
+    @Query("SELECT new sopt.uber.api.dto.res.TaxiRes(t.id, t.type, t.min, t.max, t.guests, t.comment) " +
+            "FROM Taxi t WHERE t.id IN (2, 3)")
+    List<TaxiRes> findAllCaseTaxiListResponse();
 }
